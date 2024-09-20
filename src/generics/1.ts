b5e2ea9ac9b5549c65adffcacd8e895eb1b3390f
shram <shram2615@gmail.com>
2024-09-20 10:24:07 +0300

    fix

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,37 +1,10 @@
+import axios from 'axios';
+
 async function fetchData<T>(url: string): Promise<T> {
   try {
-   
-    const mockResponse = {
-      data: {
-       
-        name: 'John Doe',
-        age: 30
-      }
-    };
-
-   
-    return mockResponse.data as T;
+    const response = await axios.get<T>(url);
+    return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
-
-
-fetchData<{ name: string; age: number }>('mock-url')
-  .then(data => {
-    console.log(data.name); 
-    console.log(data.age);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-
-fetchData<{ id: number; title: string }>('mock-url')
-  .then(data => {
-    console.log(data.id); 
-    console.log(data.title);
-  })
-  .catch(error => {
-    console.error(error);
-  });
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,17 +1,3 @@
-function merge<T, U>(objA: T, objB: U): T & U {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
 }
-
-const objA = {
-  name: 'Alice',
-  age: 30
-};
-
-const objB = {
-  position: 'Developer',
-  company: 'TechCorp'
-};
-
-
-const mergedObject = merge(objA, objB);
-console.log(mergedObject);
